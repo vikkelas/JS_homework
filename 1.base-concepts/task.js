a65e6d18ea76f637842d4822f9ccd68f0c5e07fd
Sergey Latyshev <balbesjr@gmail.com>
2021-06-25 11:31:55 +0300

    доработка домашенго задания 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,17 @@
 "use strick"
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = Math.pow(b, 2) - 4*a*c;
+  const d = Math.pow(b, 2) - 4*a*c;
 
-    if (d < 0) {
-      arr=[];
-  } else if (d === 0) {
-      let x = -b/(2*a);
+    if (d === 0) {
+      const x = -b/(2*a);
       arr.push(x);
   } else if (d > 0) {
-    let x1 = (-b + Math.sqrt(d)) / (2*a);
-    let x2 = (-b - Math.sqrt(d)) / (2*a);
+    const x1 = (-b + Math.sqrt(d)) / (2*a);
+    const x2 = (-b - Math.sqrt(d)) / (2*a);
     arr.push(x1, x2)
+  } else {
+    return arr;
   };
 
   // код для задачи №1 писать здесь
@@ -23,11 +23,12 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let originalAmount = Number(contribution);
   let loanAmount = Number(amount);
     if (Number.isNaN(interestRate) == true) {
-      totalAmount = `Параметр "процентная ставка" содержит неправильное значение ${percent}`;
+      totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
     } else if (Number.isNaN(originalAmount) == true) {
-      totalAmount = `Параметр "начальный взнос" содержит неправильное значение ${contribution}`;
+      totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+      
     } else if (Number.isNaN(loanAmount) == true) {
-      totalAmount = `Параметр "общая стоимость" содержит неправильное значение ${amount}`;
+      totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
     } else {
       let credit = loanAmount - originalAmount;
       let month;
@@ -35,10 +36,9 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
           month -= new Date().getMonth() + 1;
           month += date.getMonth() + 1;
       
-      let intRateMonth = (interestRate / 12);
-      totalAmount = credit * (Number(intRateMonth) + Number(intRateMonth) / (Math.pow((1 + Number(intRateMonth)),month) - 1))
-    }
-  // код для задачи №2 писать здесь
-
+      let intRateMonth = (interestRate / 100) / 12;
+      let montPay = credit * (Number(intRateMonth) + Number(intRateMonth) / (Math.pow((1 + Number(intRateMonth)),month) - 1));
+      totalAmount = parseFloat((montPay * month).toFixed(2));
+    }   
   return totalAmount;
 }
