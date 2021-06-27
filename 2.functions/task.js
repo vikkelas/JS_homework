469a360f8df03da52082135aaca4d4e35608f55d
Sergey Latyshev <balbesjr@gmail.com>
2021-06-27 14:04:18 +0300

    home work 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,48 @@
 // Задание 1
 function getArrayParams(arr) {
   let min,max,sum,avg;
+  min = 101;
+  max = -101;
+  sum = 0;  
+  for(let i = 0; i < arr.length; i += 1) { 
+  if(arr[i] > max) {max = arr[i]};
+  if(arr[i] < min) {min = arr[i]};
+    sum += arr[i];
+    avg = (sum / arr.length).toFixed(2);
+
+  }
 
   // Ваш код
-
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for(let i = 0; i < arr.length; i += 1){
+    sum += arr[i];  
+}
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+for(let i  = 0; i < arrOfArr.length; i += 1) {
+  let sum = func(arrOfArr[i])
+if(sum > max) {max = sum}
+}
   return max
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min,max,diff;
+  min = 100;
+  max = -101;
+  for(let i = 0; i < arr.length; i += 1) { 
+  if(arr[i] > max) {max = arr[i]};
+  if(arr[i] < min) {min = arr[i]};
+  diff = Math.abs(max-min);
+}
+return diff;
 }
