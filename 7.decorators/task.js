d4a17a202bb0d2628093a53e4b00048388049dc3
Sergey Latyshev <balbesjr@gmail.com>
2021-07-22 23:26:58 +0300

    final decoration

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,48 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper (...args) {
+    const hash = args.join(',')
+    let idx = cache.findIndex((item)=> item.hash === hash)
+    if(idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args);
+      cache.push({hash: args.join(','), value: result})
+      if(cache.length > 5) {
+        cache.splice(0,1);
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+function debounceDecoratorNew(func, ms) {
+    let checkFunction = false;
+    return function() {
+      if (checkFunction == false) {
+        func();
+        checkFunction = true;
+        setTimeout(() => (checkFunction = false), ms);
+      }      
+  };
+};
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func) {  
+  let checkFunction = false;
+    function wrapper() {
+      let index = index + 1;
+      if (checkFunction == false) {
+        func();
+        checkFunction = true;
+        wrapper.count.push(index)
+        setTimeout(() => (checkFunction = false), ms);
+      }      
+  };
+  wrapper.count = [];
+  return wrapper
 }
